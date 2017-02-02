a8a13548fa184a7b3da36fd4a83b8dceaf5a378f
Faruk Mustafic <fmc16001@student.mdh.se>
2017-02-02 04:33:30 +0100

    add parallelism

--- fuzzy-inference.py
+++ fuzzy-inference.py
@@ -1,33 +1,40 @@
 from operator import mul
 from functools import reduce
 import random
+import logging
+from collections import defaultdict
+from functools import partial
 
+logging.basicConfig(level=logging.DEBUG)
+
+def triangle(center, width, name, x):
+	if x == 'name':
+		return name
+	else:
+		x = float(x)
+
+		r = width / 2
+		k = 1 / r
+		
+		left = center - r
+		right = center + r
+
+		if left <= x <= center:
+			return k * (x - left) + 0
+		elif center <= x <= right:
+			return -k * (x - center) + 1
+		else:
+			return 0
 
 def triangular(center, width, name='x'):
-	def triangle(x):
-		if x == 'name':
-			return name
-		else:
-			x = float(x)
-
-			r = width / 2
-			k = 1 / r
-			
-			left = center - r
-			right = center + r
-
-			if left <= x <= center:
-				return k * (x - left) + 0
-			elif center <= x <= right:
-				return -k * (x - center) + 1
-			else:
-				return 0
-
-	return triangle
+	return partial(triangle, center, width, name)
 
 def labels(rng, label_count):
 	L = rng[1] - rng[0]
 	w = 2 * L / (label_count + 1)
+
+	if w == 0:
+		w = 1 # hacky
 
 	return (
 		[triangular( rng[0] + (i + 1) * w / 2, w, str(i) ) for i in range(label_count)]
@@ -75,12 +82,44 @@ def update_weights(rb, examples):
 	for r in rb:
 		r[2] = weight(r, examples)
 
-def remove_duplicates(rb)
-	return
+def update_weight(r, examples):
+	r[2] = weight(r, examples)
+	return r
+
+def rule_desc(rule):
+	rule_desc_str = ""
+
+	for f in rule[0]:
+		rule_desc_str += f('name')
+
+	return rule_desc_str
+
+def remove_duplicates(rb):
+	rule_map = defaultdict(list)
+	for r in rb:
+		rule_map[rule_desc(r)].append(r)
+		rule_map[rule_desc(r)] = [ max(rule_map[rule_desc(r)], key=lambda x: x[2]) ]
+
+	return [rule_map[k][0] for k in rule_map]
+
+	best_rules = []
+	
+	for k in rule_map:
+		best_rules.append(max(rule_map[k], key=lambda x: x[2]))
+
+	rb = best_rules
+
+from multiprocessing import Pool
+
+from itertools import product
+from multiprocessing import Pool
 
 def generate_rb(examples, db):
-	rb = [rule(e, db) for e in examples]
-	update_weights(rb, examples)
+	rb = None
+	with Pool(processes=4) as pool:
+		rb = pool.starmap(rule, [ (e, db) for e in examples ])
+		rb = pool.starmap(update_weight, [ (r, list(examples)) for r in rb ] )
+	
 	remove_duplicates(rb)
 
 	return rb
@@ -103,9 +142,9 @@ def example_from_line(line, attribute_indices, class_index):
 
 	return example
 
-def load_csv_data(file_name, attribute_indices, class_index):
+def load_csv_data(file_name, attribute_indices, class_index, line_cnt):
 	with open(file_name) as f:
-		content = f.readlines()
+		content = head = [next(f) for x in range(line_cnt)]
 		lines = [x.strip() for x in content]
 		data = [
 			example_from_line(l,attribute_indices, class_index)
@@ -150,6 +189,10 @@ class Test:
 			if ismethod(attribute) and attribute.__name__.startswith('test'):
 				print(attribute.__name__)
 				attribute()
+
+		print("")
+		print("")
+		print("")
 
 	def test_triangular(self):
 		f = triangular(1, 2)
@@ -248,12 +291,38 @@ class Test:
 	def test_find_ranges(self):
 		return
 
+	def test_rule_desc(self):
+		return
+
+	def test_remove_duplicates(self):
+		return
+
 
 test_suite = Test()
 test_suite.main()
 
-data = load_csv_data("haberman.dat", range(3), 3)
+def print_rb(rb, rw_pos = 3):
+	for rule in rb:
+		rule_str = ''
+		i = 0
+		for f in rule[0]:
+			rule_str +=  'x' + str(i) + ' is ' + f('name') + '   '
+			i += 1
+		rule_str += " class is " + str(rule[1]) + " with rw " + str(round(rule[2], rw_pos))
+		print(rule_str)
+
+logging.info("Loading data...")
+cols = [0, 4, 5, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25, 26, 27, 28, 29, 
+	30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+data = load_csv_data(
+	"kddcupfull.data", 
+	cols,
+	41,
+	2000
+)
+logging.info("Data loaded")
 random.shuffle(data)
+data = data[-2000:]
 
 validation_data_perc = 0.1
 validation_examples = int(validation_data_perc * len(data))
@@ -261,23 +330,24 @@ validation_examples = int(validation_data_perc * len(data))
 training_data = data[:-validation_examples]
 verification_data = data[validation_examples:]
 
+logging.info("Generating db...")
 label_count = 3
-ranges = find_ranges(data, [0, 1, 2])
+ranges = find_ranges(data, range(len(cols)))
 db = generate_db(ranges, label_count)
 
+logging.info("Generating rb...")
 rb = generate_rb(training_data, db)
 
-for rule in rb:
-	rule_str = ''
-	i = 0
-	for f in rule[0]:
-		rule_str +=  'x' + str(i) + ' is ' + f('name') + '   '
-		i += 1
-	print(rule_str)
-
+logging.info("Classifying...")
 total = 0
-for verification in verification_data:
-	if classify(verification[0], rb) == str(verification[1]):
-		total += 1
 
-print( "Accuracy% " + str(100 * total / len(verification_data)) )
+with Pool(processes=4) as pool:
+	classifications = pool.starmap(classify, [ (v, list(rb)) for v in verification_data])
+
+print(classifications)
+
+# for verification in verification_data:
+# 	if classify(verification[0], rb) == str(verification[1]):
+# 		total += 1
+
+# print( "Accuracy% " + str(100 * total / len(verification_data)) )
