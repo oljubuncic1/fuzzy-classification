e0dd619cd0052ce04005f32a5c90a662a790abe0
tms1337 <faruk1337@gmail.com>
2017-03-06 01:51:28 +0100

    add feature bagging

--- python/fuzzy_inference.py
+++ python/fuzzy_inference.py
@@ -51,37 +51,51 @@ def covtype_data_properties():
     cols = range(54)
     class_col = 54
 
-    row_cnt = 1000
-    data_n = 1000
+    row_cnt = 580 * 10 ** 3
+    data_n = 10 ** 3
+    def filter_f(x):
+        return x[1] == "1" or x[1] == "2"
 
-    data_properties = dl.DataProperties(file_name, cols, class_col, row_cnt, data_n)
+    data_properties = dl.DataProperties(file_name, cols, class_col, row_cnt, data_n, filter_fun=filter_f)
 
     return data_properties
 
-def random_with_replacement(data, sample_n):
-    sample = set()
+def random_features(feature_n, sample_n):
+    rand_features = []
+    
+    features = list( range(feature_n) )
     for i in range(sample_n):
-        ind = int(random.random() * len(data))
-        sample.add( data[ind] )
+        rand_i = int( random.random() * len(features) )
+        rand_features.append( features[rand_i] )
 
-    return [list(s) for s in sample]
+    return rand_features
 
-def get_classifier(training_data, ranges):
-    data_sample = random_with_replacement( training_data, 500 )
-    clf = ffc.FastFuzzyClassifier(data_sample, ranges)
+def specific_features(d, features):
+    return tuple( [ d[i] for i in features ] )
+
+def extract_features(data, ranges, features):
+    extracted_data = [ ( specific_features(d[0], features), d[1] ) for d in data ]
+    extracted_ranges = [ ranges[i] for i in features ]
+
+    return extracted_data, extracted_ranges
+
+def random_classifier_and_features(training_data, ranges, feature_n):
+    features = random_features(len(training_data[0][0]), feature_n)
+    data_sample, ranges_sample = extract_features(training_data, ranges, features)
+    clf = ffc.FastFuzzyClassifier(data_sample, ranges_sample)
     clf.fit()
 
-    return clf
+    return clf, features
 
 def get_accuracy(t, classifiers):
     avg = { "1": 0, "2" : 0 }
     for clf in classifiers:
-        prediction = clf.predict(t[0])
-        # logger.debug("\t" + str(prediction))
+        prediction = clf[0].predict( specific_features(t[0], clf[1]) )
         avg["1"] += prediction["1"]
         avg["2"] += prediction["2"]
 
     winner = max(avg)
+    print(avg[winner])
     if avg[winner] != 0.0 and winner == t[1]:
         return 1
     else:
@@ -90,7 +104,7 @@ def get_accuracy(t, classifiers):
 def main():
     verification_data_perc = 0.1
 
-    data_properties = poker_data_properties()
+    data_properties = covtype_data_properties()
     data_loader_instance = dl.DataLoader(data_properties)
     data_loader_instance.set_logger(logger)
     data_loader_instance.load(shuffle=True)
@@ -102,24 +116,35 @@ def main():
     training_data = data[:-verification_data_n]
     verification_data = data[-verification_data_n:]
 
-    for classifier_n in [ int(2 ** i) for i in [6] ]:
+    for classifier_n in [ int(2 ** i) for i in [0, 1, 2] ]:
         logger.debug("Using " + str(classifier_n) + " classifiers")
         classifiers = []
+        feature_n = int( 10 )
         with Pool(processes=4) as pool:
-            classifiers = pool.starmap( get_classifier, [ (data, ranges) for i in range(classifier_n) ] )
+            parameters =  [ (training_data, ranges, feature_n) for i in range(classifier_n) ]
+            classifiers = pool.starmap( random_classifier_and_features, parameters)
 
         logger.debug("\tClassifying...")
 
         with Pool(processes=4) as pool:
-            acc_list = pool.starmap( get_accuracy, [ (t, classifiers) for t in verification_data ] )
+            acc_list = pool.starmap( get_accuracy, [ (c, classifiers) for c in verification_data ] )
 
         acc = len([a for a in acc_list if a == 1]) / len(verification_data)
-        logger.debug( "\tAccuracy% " + str(acc) )
+        logger.debug( "\tAccuracy " + str(acc) )
 
-    logger.debug("Classifying with full data...")
-    clf = ffc.FastFuzzyClassifier(training_data, ranges)
-    clf.fit()
-    logger.debug(clf.evaluate(verification_data))
+    logger.debug("Class distribution")
+    for c in set( [ x[1] for x in verification_data ] ):
+        in_class_n = len( [x for x in verification_data if x[1] == c] )
+        logger.debug(
+            "\tData items with class " + c + " -> " + str( in_class_n ) + \
+            "\t percentage " + str( in_class_n / len(verification_data) )
+        )
+
+    # logger.debug("Classifying with full data...")
+    # clf = ffc.FastFuzzyClassifier(training_data, ranges)
+    # clf.set_logger(logger)
+    # clf.fit()
+    # logger.debug(clf.evaluate(verification_data))
 
 if __name__ == "__main__":
     set_logger()
