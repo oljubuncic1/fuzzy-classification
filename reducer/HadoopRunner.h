6f2e74e7f9a3b718f70e8ce38d01b3cd10a7827a
tms1337 <faruk1337@gmail.com>
2017-04-30 19:10:53 +0200

    prepare for debuging with data_properties on only

--- reducer/HadoopRunner.h
+++ reducer/HadoopRunner.h
@@ -69,21 +69,21 @@ public:
                         categorical_features,
                         line_cnt);
 
-//        vector<example_t> verification_str_data = load_csv_data(verification_data_path,
-//                                                            attribute_inds,
-//                                                            class_ind,
-//                                                            line_cnt,
-//                                                            separation_char);
-//
-//        data_t verification_data;
-//        for (auto &x : verification_str_data) {
-//            vector<double> item;
-//            for (auto val : x.first) {
-//                item.push_back(to_double(val));
-//            }
-//            string classification = x.second;
-//            verification_data.push_back(make_pair(item, classification));
-//        }
+        vector<example_t> verification_str_data = load_csv_data(verification_data_path,
+                                                            attribute_inds,
+                                                            class_ind,
+                                                            line_cnt,
+                                                            separation_char);
+
+        data_t verification_data;
+        for (auto &x : verification_str_data) {
+            vector<double> item;
+            for (auto val : x.first) {
+                item.push_back(to_double(val));
+            }
+            string classification = x.second;
+            verification_data.push_back(make_pair(item, classification));
+        }
 
         int curr_id = -1;
         vector<example_t> curr_data;
@@ -99,43 +99,44 @@ public:
             ss >> id;
 
             if(id == curr_id) {
-//                example_t curr_example;
-//                curr_example = example_from_line(example_str,
-//                                                 attribute_inds,
-//                                                 class_ind,
-//                                                 separation_char);
-//
-//                curr_data.push_back(curr_example);
+                example_t curr_example;
+                curr_example = example_from_line(example_str,
+                                                 attribute_inds,
+                                                 class_ind,
+                                                 separation_char);
+
+                curr_data.push_back(curr_example);
             } else {
-                cout << 1 << "\t" << 1 << endl;
-//                // build a tree for prev and classify
-//                vector<range_t> ranges;
-//                find_ranges(curr_data, ranges);
+//                if(id != -1) {
+//                    // build a tree for prev and classify
+//                    vector<range_t> ranges;
+//                    find_ranges(curr_data, ranges);
 //
-//                data_t data;
-//                for (auto &x : curr_data) {
-//                    vector<double> item;
-//                    for (auto val : x.first) {
-//                        item.push_back(to_double(val));
+//                    data_t data;
+//                    for (auto &x : curr_data) {
+//                        vector<double> item;
+//                        for (auto val : x.first) {
+//                            item.push_back(to_double(val));
+//                        }
+//                        string classification = x.second;
+//                        data.push_back(make_pair(item, classification));
 //                    }
-//                    string classification = x.second;
-//                    data.push_back(make_pair(item, classification));
+//
+//                    RandomFuzzyForest rff(tree_n);
+//                    rff.fit(data,
+//                            ranges,
+//                            categorical_features,
+//                            numerical_features);
+//
+////                    for(auto &v : verification_data) {
+////                        auto membs = rff.predict_memberships(v);
+////                        cout << v << "\t" << membs << endl;
+////                    }
 //                }
-//
-//                RandomFuzzyForest rff(tree_n);
-//                rff.fit(data,
-//                        ranges,
-//                        categorical_features,
-//                        numerical_features);
-//
-////                for(auto &v : verification_data) {
-////                    auto membs = rff.predict_memberships(v);
-////                    cout << v << "\t" << membs << endl;
-////                }
-//
-//                // start a new
-//                curr_id = id;
-//                curr_data = vector<example_t>();
+
+                // start a new
+                curr_id = id;
+                curr_data = vector<example_t>();
             }
         }
 
