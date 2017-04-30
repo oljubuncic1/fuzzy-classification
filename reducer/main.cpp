ea4d97f28d8b8dfa7995d6d3491b7ac2ea4bd567
tms1337 <faruk1337@gmail.com>
2017-04-30 17:34:00 +0200

    change dataset in reducer

--- reducer/main.cpp
+++ reducer/main.cpp
@@ -20,7 +20,7 @@ int main() {
 
     int reducer_n = 5;
     string verification_data_path = "./testdat";
-    string big_dataset = "";
+    string big_dataset = "WIN";
     int tree_n = 10;
 
     int clasifier_n = 100;
