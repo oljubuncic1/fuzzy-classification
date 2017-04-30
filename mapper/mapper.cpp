b6e2cc63654df00400930ad2e4570475ae31e115
tms1337 <faruk1337@gmail.com>
2017-04-30 13:12:35 +0200

    remove old files

---
+++ mapper/mapper.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define REDUCER_N = 5;
+
+int main() {
+
+    string line;
+    int i = 0;
+	while (getline(cin, line)) {
+		cout << i << "\t" << line << endl;
+        i = (i + 1) % REDUCER_N;
+	}
+
+    return 0;
+}
