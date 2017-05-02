ed693b96f7442b9770538c379e37092959fe059a
tms1337 <faruk1337@gmail.com>
2017-05-02 18:01:04 +0200

    debugging

--- result_reducer/reducer.py
+++ result_reducer/reducer.py
@@ -2,30 +2,31 @@ from operator import itemgetter
 import sys
 import ast
 
-curr_k = None
-curr_predictions = {}
+# curr_k = None
+# curr_predictions = {}
 
 # input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    pass
+    # # remove leading and trailing whitespace
+    # line = line.strip()
 
-    # parse the input we got from mapper.py
-    k, v = line.split('\t', 1)
+    # # parse the input we got from mapper.py
+    # k, v = line.split('\t', 1)
 
-    if k == curr_k:
-        # continue
-        m = ast.literal_eval(v)
-        for label in m:
-            if not label in curr_predictions:
-                curr_predictions[label] = 0
+    # if k == curr_k:
+    #     # continue
+    #     m = ast.literal_eval(v)
+    #     for label in m:
+    #         if not label in curr_predictions:
+    #             curr_predictions[label] = 0
             
-            curr_predictions[label] += m[label]
-    else:
-        if curr_k is not None:
-            # new key arrived, print results
-            # for previous
-            print(curr_k, "\t", curr_predictions)
+    #         curr_predictions[label] += m[label]
+    # else:
+    #     if curr_k is not None:
+    #         # new key arrived, print results
+    #         # for previous
+    #         print(curr_k, "\t", curr_predictions)
             
-        curr_k = k
-        curr_predictions = m
+    #     curr_k = k
+    #     curr_predictions = m
