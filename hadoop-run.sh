669a1e7b59b2284be32058b133f6d7a83b0c5172
tms1337 <faruk1337@gmail.com>
2017-05-02 19:29:17 +0200

    debugging

--- hadoop-run.sh
+++ hadoop-run.sh
@@ -1,4 +1,4 @@
-$HADOOP_PREFIX/bin/hadoop fs -rm -R -F output* resultoutput*
+$HADOOP_PREFIX/bin/hadoop fs -rm -R -f output* resultoutput*
 
 git pull && /opt/rh/devtoolset-2/root/usr/bin/c++ --std=c++11 mapper/mapper.cpp -o /usr/local/bin/mapper && /opt/rh/devtoolset-2/root/usr/bin/c++ --std=c++11 reducer/main.cpp reducer/*.h -o /usr/local/bin/reducer && $HADOOP_PREFIX/bin/hadoop jar /usr/local/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar  -D mapreduce.job.reduces=3 -mapper /usr/local/bin/mapper -reducer /usr/local/bin/reducer -input datasets -output output000 && /opt/rh/devtoolset-2/root/usr/bin/c++ --std=c++11 result_reducer/reducer.cpp -o /usr/local/bin/result_reducer && $HADOOP_PREFIX/bin/hadoop jar /usr/local/hadoop-2.7.1/share/hadoop/tools/lib/hadoop-streaming-2.7.1.jar  -D mapreduce.job.reduces=3 -mapper /bin/cat -reducer /usr/local/bin/result_reducer -input output000 -output resultoutput000
 
