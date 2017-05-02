1677d1e2380d106ad9945f5db795881eebfcc07f
tms1337 <faruk1337@gmail.com>
2017-05-02 15:01:40 +0200

    debugging

--- mapper/mapper.cpp
+++ mapper/mapper.cpp
@@ -10,14 +10,16 @@ int main() {
     string line;
     int i = 0;
 	while (getline(cin, line)) {
-        if(line[0] == 't') {
-            for(int j = 0; j < REDUCER_N; j++) {
-                cout << j << "\t" << line << endl;
+        if(find(line.begin(), line.end(), '?') == line.end()) {
+            if(line[0] == 't') {
+                for(int j = 0; j < REDUCER_N; j++) {
+                    cout << j << "\t" << line << endl;
+                }
+            } else {
+                cout << i << "\t" << line << endl;
             }
-        } else {
-		    cout << i << "\t" << line << endl;
+            i = (i + 1) % REDUCER_N;
         }
-        i = (i + 1) % REDUCER_N;
 	}
 
     return 0;
