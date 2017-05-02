5624eeaeb01005367deac4ad6a9f28e1e628ed7e
tms1337 <faruk1337@gmail.com>
2017-05-02 17:51:46 +0200

    debugging

--- result_reducer/reducer.py
+++ result_reducer/reducer.py
@@ -9,24 +9,25 @@ curr_predictions = {}
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    print(line)
 
     # parse the input we got from mapper.py
-    k, v = line.split('\t', 1)
+    # k, v = line.split('\t', 1)
     # m = ast.literal_eval(v)
-    print(k, "\t", v)
+    # print(k, "\t", v)
 
-    if k == curr_k:
+    # if k == curr_k:
         # continue
         # for label in m:
         #     if not label in curr_predictions:
         #         curr_predictions[label] = 0
             
         #     curr_predictions[label] += m[label]
-    else:
+    # else:
         # if curr_k is not None:
         #     # new key arrived, print results
         #     # for previous
         #     print(curr_k, "\t", curr_predictions)
             
-        curr_k = k
-        curr_predictions = m
+        # curr_k = k
+        # curr_predictions = m
