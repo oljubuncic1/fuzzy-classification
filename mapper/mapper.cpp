2270fde5e1d343adc9fce4fdd9c9ba12a213ca05
tms1337 <faruk1337@gmail.com>
2017-05-02 15:03:07 +0200

    debugging

--- mapper/mapper.cpp
+++ mapper/mapper.cpp
@@ -10,7 +10,7 @@ int main() {
     string line;
     int i = 0;
 	while (getline(cin, line)) {
-        if(line.find("?") == line.end()) {
+        if(line.find("?") == std::string::npos) {
             if(line[0] == 't') {
                 for(int j = 0; j < REDUCER_N; j++) {
                     cout << j << "\t" << line << endl;
