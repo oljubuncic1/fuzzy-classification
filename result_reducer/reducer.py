ee4a59a8c7687e400b3d06564ff9efe3f3d4bbc0
tms1337 <faruk1337@gmail.com>
2017-05-02 17:42:59 +0200

    debugging

---
+++ result_reducer/reducer.py
@@ -0,0 +1,31 @@
+from operator import itemgetter
+import sys
+import ast
+
+curr_k = None
+curr_predictions = {}
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    k, v = line.split('\t', 1)
+    m = ast.literal_eval(v)
+
+    if k == curr_k:
+        # continue
+        for label in m:
+            if not label in curr_predictions:
+                curr_predictions[label] = 0
+            
+            curr_predictions[label] += m[label]
+    else:
+        if curr_k is not None:
+            # new key arrived, print results
+            # for previous
+            print(curr_k, "\t", curr_predictions)
+            
+        curr_k = k
+        curr_predictions = m
