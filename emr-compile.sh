30f0df7111ff7438f43e424b87c4c465def0d9c2
tms1337 <faruk1337@gmail.com>
2017-05-04 18:07:02 +0200

    emr debugging

--- emr-compile.sh
+++ emr-compile.sh
@@ -1,4 +1,4 @@
 g++ --std=c++11 data-distributor/mapper.cpp -o /usr/local/bin/measurementsbin/step-1-mapper
 g++ --std=c++11 mapper/mapper.cpp -o /usr/local/bin/measurementsbin/step-2-mapper
 g++ --std=c++11 reducer/mainr.cpp reducer/*.h -o /usr/local/bin/measurementsbin/step-2-reducer
-g++ --std=c++11 result-reducer/reducer.cpp -o /usr/local/bin/measurementsbin/step-3-reducer
+g++ --std=c++11 result_reducer/reducer.cpp -o /usr/local/bin/measurementsbin/step-3-reducer
