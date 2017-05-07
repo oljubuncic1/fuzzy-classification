3784a62ca53ef812a9536c80187ef477b5efe4ae
tms1337 <faruk1337@gmail.com>
2017-05-07 20:13:21 +0200

    emr debugging

--- emr-count-correct.sh
+++ emr-count-correct.sh
@@ -1,5 +1,5 @@
-$1 = hadoop fs -cat s3://farukintermediate/step-3-output/* | wc -l}
-$2 = hadoop fs -cat s3://farukintermediate/step-1-output/* | grep -o 't' | wc -l}
+$a = hadoop fs -cat s3://farukintermediate/step-3-output/* | wc -l}
+$b = hadoop fs -cat s3://farukintermediate/step-1-output/* | grep -o 't' | wc -l}
 
-echo $1
-echo $2
+echo $a
+echo $b
