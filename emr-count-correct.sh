adb66062a203f2e119250242446fd2a040190438
tms1337 <faruk1337@gmail.com>
2017-05-07 18:45:00 +0200

    emr debugging

--- emr-count-correct.sh
+++ emr-count-correct.sh
@@ -1,1 +1,4 @@
-hadoop fs -cat s3://farukintermediate/step-3-output/* | wc -l
+$1 = ${hadoop fs -cat s3://farukintermediate/step-3-output/* | wc -l}
+$2 = ${hadoop fs -cat s3://farukintermediate/step-1-output/* | grep -o 't' | wc -l}
+
+echo "print 1.0 * $1 / $2" > python
