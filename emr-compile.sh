75fa1b40bf357a6e215c5c41db21e761350af631
tms1337 <faruk1337@gmail.com>
2017-05-08 15:42:19 +0200

    emr debugging

--- emr-compile.sh
+++ emr-compile.sh
@@ -3,8 +3,5 @@ g++ --std=c++11 -O3 mapper/mapper.cpp -o /usr/local/bin/measurementsbin/step-2-m
 g++ --std=c++11 -O3 reducer/main.cpp reducer/*.h -o /usr/local/bin/measurementsbin/step-2-reducer
 g++ --std=c++11 -O3 result_reducer/reducer.cpp -o /usr/local/bin/measurementsbin/step-3-reducer
 
-hadoop fs -rm -R -f s3://farukbin/bin/*
-hadoop fs -rm -R -f s3://farukintermediate/step-1-output
-hadoop fs -rm -R -f s3://farukintermediate/step-2-output
-hadoop fs -rm -R -f s3://farukintermediate/step-3-output
+
 hadoop fs -put /usr/local/bin/measurementsbin/* s3://farukbin/bin
